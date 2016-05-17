655e01ca223a428531224e9248fba18767c6bc5e
bgalbraith <byron.galbraith@gmail.com>
2016-05-17 09:27:49 -0700

    environmnet object than runs experiments and provides some results plotting methods

--- bandits/environment.py
+++ bandits/environment.py
@@ -1,8 +1,9 @@
-import matplotlib
-matplotlib.use('qt4agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import scipy.stats as stats
+
+from bandits.agent import BetaAgent
 
 
 class Environment(object):
@@ -36,6 +37,7 @@ class Environment(object):
 
     def plot_results(self, scores, optimal):
         sns.set_style('white')
+        sns.set_context('talk')
         plt.subplot(2, 1, 1)
         plt.title(self.label)
         plt.plot(scores)
@@ -47,5 +49,49 @@ class Environment(object):
         plt.ylabel('% Optimal Action')
         plt.xlabel('Time Step')
         plt.legend(self.agents, loc=4)
+        sns.despine()
+        plt.show()
+
+    def plot_beliefs(self):
+        sns.set_context('talk')
+        pal = sns.color_palette("cubehelix", n_colors=len(self.agents))
+        plt.title(self.label + ' - Agent Beliefs')
+
+        rows = 2
+        cols = int(self.bandit.k / 2)
+
+        axes = [plt.subplot(rows, cols, i+1) for i in range(self.bandit.k)]
+        for i, val in enumerate(self.bandit.action_values):
+            color = 'r' if i == self.bandit.optimal else 'k'
+            axes[i].vlines(val, 0, 1, colors=color)
+
+        for i, agent in enumerate(self.agents):
+            if type(agent) is not BetaAgent:
+                for j, val in enumerate(agent.value_estimates):
+                    axes[j].vlines(val, 0, 0.75, colors=pal[i], alpha=0.8)
+            else:
+                x = np.arange(0, 1, 0.001)
+                y = np.array([stats.beta.pdf(x, a, b) for a, b in
+                             zip(agent.alpha, agent.beta)])
+                y /= np.max(y)
+                for j, _y in enumerate(y):
+                    axes[j].plot(x, _y, color=pal[i], alpha=0.8)
+
+        min_p = np.argmin(self.bandit.action_values)
+        for i, ax in enumerate(axes):
+            ax.set_xlim(0, 1)
+            if i % cols != 0:
+                ax.set_yticklabels([])
+            if i < cols:
+                ax.set_xticklabels([])
+            else:
+                ax.set_xticks([0, 0.25, 0.5, 0.75, 1.0])
+                ax.set_xticklabels(['0', '', '0.5', '', '1'])
+            if i == int(cols/2):
+                title = '{}-arm Bandit - Agent Estimators'.format(self.bandit.k)
+                ax.set_title(title)
+            if i == min_p:
+                ax.legend(self.agents)
+
         sns.despine()
         plt.show()
