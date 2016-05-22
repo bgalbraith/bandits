60d36470588c9c1f21814a433970223a2769c58b
bgalbraith <byron.galbraith@gmail.com>
2016-05-22 13:43:56 -0400

    binomial bandit example

--- examples/bayesian.py
+++ examples/bayesian.py
@@ -1,23 +1,42 @@
 """
 Takes advantage of multicore systems to speed up the simulation runs.
 """
+import matplotlib
+matplotlib.use('qt4agg')
 from bandits.agent import Agent, BetaAgent
-from bandits.bandit import BernoulliBandit
+from bandits.bandit import BernoulliBandit, BinomialBandit
 from bandits.policy import GreedyPolicy, EpsilonGreedyPolicy, UCBPolicy
 from bandits.environment import Environment
 
 
-if __name__ == '__main__':
-    experiments = 500
-    trials = 1000
-
+class BernoulliExample:
+    label = 'Bayesian Bandits - Bernoulli'
     bandit = BernoulliBandit(10, t=3*1000)
     agents = [
         Agent(bandit, EpsilonGreedyPolicy(0.1)),
         Agent(bandit, UCBPolicy(1)),
         BetaAgent(bandit, GreedyPolicy())
     ]
-    env = Environment(bandit, agents, label='Bayesian Bandits')
+
+
+class BinomialExample:
+    label = 'Bayesian Bandits - Binomial (n=5)'
+    bandit = BinomialBandit(10, n=5, t=3*1000)
+    agents = [
+        Agent(bandit, EpsilonGreedyPolicy(0.1)),
+        Agent(bandit, UCBPolicy(1)),
+        BetaAgent(bandit, GreedyPolicy())
+    ]
+
+
+if __name__ == '__main__':
+    experiments = 500
+    trials = 1000
+
+    example = BernoulliExample()
+    # example = BinomialExample()
+
+    env = Environment(example.bandit, example.agents, example.label)
     scores, optimal = env.run(trials, experiments)
     env.plot_results(scores, optimal)
     env.plot_beliefs()
